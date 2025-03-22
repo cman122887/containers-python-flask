3de520ae5433e903bffa55d5893a9b51b31770f2
cman122887 <cman122887@hotmail.com>
2025-03-22 12:37:58 -0700

    Update app.py
    typo in post call

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def cts():
     print(rj)
     if rj['text'].lower().strip() == 'dice roll':
         ret_msg = {'bot_id': '7d22d6aae5e37b72fbb41ef03b', 'text': str(random.randint(1,6))}
-        requsts.post('https://api.groupme.com/v3/bots/post', json=ret_msg)
+        requests.post('https://api.groupme.com/v3/bots/post', json=ret_msg)
         return json.dumps({'success': True}), 201, {'ContentType':'application/json'} 
     return json.dumps(rj), 200, {'ContentType':'application/json'} 
 
